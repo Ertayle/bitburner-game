10f72678d055d1df4d9ae4bed2a4cad45062bc85
Ertayle <ertayle_@hotmail.com>
2021-12-23 03:03:38 +0100

    v1.05

--- src/dlscripts.js
+++ src/dlscripts.js
@@ -1,17 +1,17 @@
 /** @param {NS} ns **/
 export async function main(ns) {
 
-    let scripts = {
+    let _scripts = {
         'hosts.js' : 'https://raw.githubusercontent.com/Ertayle/bitburner-game/main/src/hosts.js',
         'start.js' : 'https://raw.githubusercontent.com/Ertayle/bitburner-game/main/src/start.js',
     }
 
-    Object.keys(scripts).forEach( scriptName => {
-        let scriptUrl = scripts[scriptName];
+    Object.keys(_scripts).forEach( async _scriptName => {
+        let _scriptUrl = _scripts[_scriptName];
         // Force file remove
-        ns.rm(scriptName);
+        ns.rm(_scriptName);
         // Redownload script
-        await ns.wget(scriptUrl, scriptName); 
+        await ns.wget(_scriptUrl.toString(), _scriptName.toString); 
     });
 
 }
--- src/init.js
+++ src/init.js
@@ -1,6 +1,6 @@
 /** @param {NS} ns **/
 export async function main(ns) {
     await ns.wget('https://raw.githubusercontent.com/Ertayle/bitburner-game/main/src/dlscripts.js', 'dlscripts.js');
-    await ns.exec('dlscripts.js', 'home');
-    await ns.exec('start.js', 'home');
+    await ns.run('dlscripts.js');
+    await ns.run('start.js');
 }

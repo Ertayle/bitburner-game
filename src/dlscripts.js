d0f34d10bdf578f3b4328f457b5b6e9093d75f9c
Ertayle <ertayle_@hotmail.com>
2021-12-23 02:32:28 +0100

    v1.01

--- src/dlscripts.js
+++ src/dlscripts.js
@@ -1,5 +1,4 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-    ns.wget();
-    ns.wget();
+    ns.wget('https://github.com/Ertayle/bitburner-game/blob/b722360d67477aa4e7606f3623e7011dd7684913/src/hosts.js', 'hosts.js');
 }
